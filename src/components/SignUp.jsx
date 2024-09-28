0041048e3a656e8f01a7be06c6f5976df7331504
Lam Le <lam.le@swagsoft.com>
2024-09-28 14:53:04 +0700

    first commit

---
+++ src/components/SignUp.jsx
@@ -0,0 +1,79 @@
+import React, { useState, useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { Box, CardMedia } from "@mui/material";
+import { toast } from "react-toastify";
+
+import { Videos, ChannelCard } from ".";
+import { registerAPI } from "../utils/fetchFromAPI";
+
+const SignUp = () => {
+  const [channelDetail, setChannelDetail] = useState();
+  const [videos, setVideos] = useState(null);
+
+  const { id } = useParams();
+  const navigate = useNavigate();
+
+  useEffect(() => {}, []);
+
+  const handleRegister = () => {
+    const fullName = document.getElementById("fullName").value;
+    const email = document.getElementById("email").value;
+    const pass = document.getElementById("pass").value;
+
+    registerAPI({ fullName, email, pass })
+      .then((data) => {
+        console.log(data);
+        toast.success(data.message);
+        navigate("/login");
+      })
+      .catch((err) => {
+        console.log(err.response.data.message);
+        toast.error(err.response.data.message);
+      });
+  };
+
+  return (
+    <div className="p-5 " style={{ minHeight: "100vh" }}>
+      <div className=" d-flex justify-content-center">
+        <form className="row g-3 text-white">
+          {/* fullName */}
+          <div className="col-md-12">
+            <label htmlFor="inputEmail4" className="form-label">
+              Full name
+            </label>
+            <input className="form-control" id="fullName" />
+          </div>
+
+          {/* email */}
+          <div className="col-md-12">
+            <label htmlFor="inputEmail4" className="form-label">
+              Email
+            </label>
+            <input type="email" className="form-control" id="email" />
+          </div>
+
+          {/* pass */}
+          <div className="col-md-12">
+            <label htmlFor="inputEmail4" className="form-label">
+              Password
+            </label>
+            <input className="form-control" id="pass" />
+          </div>
+
+          {/* button */}
+          <div className="col-12">
+            <button
+              onClick={handleRegister}
+              type="button"
+              className="btn btn-primary"
+            >
+              Sign Up
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default SignUp;
